e85a7bea5beb687ba943bf067f574be5e2bc595b
Natalia Holkova <holkova.natali@gmail.com>
2018-03-29 22:24:22 +0200

    Added math library class

---
+++ src/Calculator/Calculator/MatLib.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Calculator
+{
+    class MatLib
+    {
+        public static double add(double number1, double number2)
+        {
+            return number1 + number2;
+        }
+
+        public static double subtract(double number1, double number2)
+        {
+            return number1 - number2;
+        }
+
+        public static double multiply(double number1, double number2)
+        {
+            return number1 * number2;
+        }
+
+        public static double divide(double number1, double number2)
+        {
+            return number1 / number2;
+        }
+
+        public static long factorial(int number)
+        {
+            long res = 1;
+            for (int i = 2; i <= number; i++)
+            {
+                res = res * i;
+            }
+            return res;
+        }
+
+        public static double square(double number)
+        {
+            return number * number;
+        }
+
+        public static double power(double number, int exp)
+        {
+            return System.Math.Pow(number, exp);
+        }
+
+        public static double sqrt(double number)
+        {
+            return System.Math.Sqrt(number);
+        }
+
+        public static double genroot(double number, int root)
+        {
+            return System.Math.Pow(number, 1.0 / root);
+        }
+
+        public static double log10(double number)
+        {
+            return System.Math.Log10(number);
+        }
+    }
+}
--- src/Calculator/Calculator/Program.cs
+++ src/Calculator/Calculator/Program.cs
@@ -16,6 +16,47 @@ namespace Calculator
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            Console.WriteLine("Calculator started!");
+
+            Console.WriteLine("Matlib test");
+
+            double n1 = 2;
+            double n2 = 40;
+            double res = MatLib.add(n1, n2);
+            Console.WriteLine("{0} + {1} = {2}", n1, n2, res);
+
+            n1 = 9;
+            n2 = 27;
+            res = MatLib.subtract(n1, n2);
+            Console.WriteLine("{0} - {1} = {2}", n1, n2, res);
+
+            n1 = 5;
+            n2 = 7;
+            res = MatLib.multiply(n1, n2);
+            Console.WriteLine("{0} * {1} = {2}", n1, n2, res);
+
+            n1 = 99;
+            n2 = 3;
+            res = MatLib.divide(n1, n2);
+            Console.WriteLine("{0} / {1} = {2}", n1, n2, res);
+
+            int num_int = 5;
+            long res_fact = MatLib.factorial(num_int);
+            Console.WriteLine("{0}! = {1}", num_int, res_fact);
+
+            double num = 0.81;
+            double res_sqrt = MatLib.sqrt(num);
+            Console.WriteLine("sqrt({0}) = {1}", num, res_sqrt);
+
+            double num_to_calc = 27;
+            int root = 3;
+            double res_genroot = MatLib.genroot(num_to_calc, root);
+            Console.WriteLine("genroot({0}, {1}) = {2}", num_to_calc, root, res_genroot);
+
+            double x = 100;
+            double res_log10 = MatLib.log10(x);
+            Console.WriteLine("log10({0}) = {1}", x, res_log10);
         }
     }
 }
