2621375ac90e41fdd4d996946b912dbe606ec6df
Filip Bali <14928466+FilipBali@users.noreply.github.com>
2018-04-06 08:48:46 +0200

    Update Program.cs

--- src/Calculator/Calculator/Program.cs
+++ src/Calculator/Calculator/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Forms;
+using System.Threading.Tasks;
 
 namespace Calculator
 {
@@ -60,3 +61,21 @@ namespace Calculator
         }
     }
 }
+
+namespace Calc_GUI
+{
+    static class GUI
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Calculator.CalculatorGUI());
+        }
+    }
+}
+
