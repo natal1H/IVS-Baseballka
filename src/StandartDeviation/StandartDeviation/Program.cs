12d17e4bc3b69db8de2cdf62b5735e9b67bc6bd2
Natalia Holkova <xholko02@stud.fit.vutbr.cz>
2018-04-11 13:52:58 +0200

    Added class to calculate standart deviation
    Added class to calculate standart deviation and created constructor for
    class.

--- src/StandartDeviation/StandartDeviation/Program.cs
+++ src/StandartDeviation/StandartDeviation/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+//using System.Linq;
+//using System.Text;
+using StandartDeviation;
 
 namespace StandartDeviation
 {
@@ -9,6 +10,19 @@ namespace StandartDeviation
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("STANDART DEVIATION");
+
+            List<double> list = new List<double> { 1.0, 2.0, 3.0 };
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write("{0} ", list[i]);
+            }
+            Console.WriteLine();
+
+            StandartDeviation s = new StandartDeviation(list);
+
+            Console.WriteLine("Press any key to continue . . .");
+            Console.Read();
         }
     }
 }
---
+++ src/StandartDeviation/StandartDeviation/StandartDeviation.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * @file StandartDeviation.cs
+ * 
+ * @brief Trieda zameraná na výpočet smerodajnej odchýlky
+ * @author Natália Holková (xholko02), Egor Shamardin (xshama00)
+ * 
+ * @note .NET Framework v4.0
+ */
+
+using System;
+using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+
+namespace StandartDeviation
+{
+    /**
+     * @class Trieda na výpočet smerodajnej odchýlky
+     */
+    class StandartDeviation
+    {
+        private List<double> elements; /** Zoznam všetkých čísiel v postupnosti */
+        private int N;
+
+        public StandartDeviation(List<double> numbers)
+        {
+            elements = numbers;
+            N = elements.Count;
+        } 
+ 
+    }
+}
